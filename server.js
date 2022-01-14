fd5738fb7a4f67f7939118e468b3db8daad6d032
Lencof <Danialusupov121@mail.ru>
2022-01-14 14:58:34 +0600

    Initial project version

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ const MongoStore = require('connect-mongo')(session);
 
 const MongoDBURI = process.env.MONGO_URI || 'mongodb+srv://Adriol:Qmozet!jhdsfrte@cluster0.nmrhx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
+
 mongoose.connect(MongoDBURI, {
   useUnifiedTopology: true,
   useNewUrlParser: true
 });
 
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-});
+db.once('open', () => {});
 
 app.use(session({
   secret: 'work hard',
@@ -34,32 +35,49 @@ app.set('view engine', 'ejs');
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public/about.html'));
-app.use(express.static(__dirname + '/routes/chatcord.js'));
+// app.use(express.static(__dirname + '/public/timer.html'));
+// app.use(express.static(__dirname + '/routes/timer.js'));
+
 
 
 const index = require('./routes/index');
 const about = require('./routes/about');
-const chatcord = require('./routes/chatcord');
+const timer = require('./routes/timer');
 
 app.use('/', index);
 app.use('/about', about);
-app.use('/chatcord', chatcord);
+// app.use('/timer', timer);
 
 
-// Chatcord page
-app.get('/chatcord', function(req, res) {
-  res.redirect('http://localhost:4000/');
+
+// // Chatcord page
+// app.get('/chatcord', function (req, res) {
+//   res.redirect('http://localhost:4000/');
+// });
+
+
+// create Timer page
+const timerPg = path.join(__dirname, "./public/timer.html"); 
+
+
+// Timer page
+app.get('/timer', function (req, res) {
+  res.sendFile(timerPg);
 });
 
 
+// create About page
+const aboutPg = path.join(__dirname, "./public/about.html");
 
 // About page
-app.use((req, res) => {
-  res.sendFile(__dirname + '/public/about.html');
+app.get("/about", function (req, res) {
+  res.sendFile(aboutPg);
 });
 
 
@@ -67,11 +85,16 @@ app.use((req, res) => {
 const errorPg = path.join(__dirname, "./public/index.html"); //this is your error page
 
 // catch 404 and forward to error handler
-app.get("*", function(req,res){
+app.get("*", function (req, res) {
   res.sendFile(errorPg);
 });
 
+
+
+
 // listen on port 3000
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Express app listening on port 3000');
+const port = 4000;
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Express app listening on port ${port}`);
 });
