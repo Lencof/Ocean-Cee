2ee1e22a44122eff1ad268478e7c44e7e7086ce2
Lencof <71154457+Lencof@users.noreply.github.com>
2022-01-22 13:16:09 +0600

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ mongoose.connect(MongoDBURI, {
   useNewUrlParser: true
 });
 
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {});
